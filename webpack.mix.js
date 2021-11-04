134fd6a738c4e7e2d13bc235d6294479d69c456a
Brandon Fenning <brandon@lform.com>
2021-11-04 02:45:52 -0400

    Adjusts webpack to no longer use sass

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,9 +1,15 @@
 let mix = require('laravel-mix');
 
-mix.sass('./scss/app.scss', 'src/app.prepost.css').postCss(
-	'./src/app.prepost.css',
-	'./dist/app.css',
-	[require('tailwindcss')]
-);
+// mix.sass('./scss/app.scss', 'src/app.prepost.css').postCss(
+// 	'./src/app.prepost.css',
+// 	'./dist/app.css',
+// 	[require('tailwindcss')]
+// );
+//
+// mix.minify('./dist/app.css');
 
-mix.minify('./dist/app.css');
+mix.postCss('./css/app.css', './dist/app.css', [
+	require('postcss-import'),
+	//require('autoprefixer'),
+	require('tailwindcss'),
+]).minify('./dist/app.css');
