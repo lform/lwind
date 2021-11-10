d7f242dfb280fa40faafc6dba1c2f8e3c90d3a93
Brandon Fenning <brandon@lform.com>
2021-11-10 08:54:47 -0500

    Removes SASS styling from webpack config

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,15 +1,13 @@
 let mix = require('laravel-mix');
 
-// mix.sass('./scss/app.scss', 'src/app.prepost.css').postCss(
-// 	'./src/app.prepost.css',
-// 	'./dist/app.css',
-// 	[require('tailwindcss')]
-// );
-//
-// mix.minify('./dist/app.css');
+mix.webpackConfig({
+	stats: {
+		children: true,
+	},
+});
 
-mix.postCss('./css/app.css', './dist/app.css', [
+mix.postCss('./css/app.pcss', './dist/app.css', [
 	require('postcss-import'),
-	//require('autoprefixer'),
+	// require('autoprefixer'),
 	require('tailwindcss'),
 ]).minify('./dist/app.css');
