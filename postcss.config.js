0925835a106f80d68cb85bef568054aeec28256f
Brandon Fenning <brandon@lform.com>
2021-11-10 09:00:50 -0500

    Updates postcss config with plugins to be loaded

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,9 @@
 module.exports = {
-	plugins: {
-		tailwindcss: {},
-		autoprefixer: {},
-	},
+	plugins: [
+		require('tailwindcss/nesting'),
+		require('postcss-import'),
+		require('postcss-advanced-variables'),
+		require('tailwindcss'),
+		require('autoprefixer'),
+	],
 };
