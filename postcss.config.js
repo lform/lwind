d2d2c94e6c3ab34485382276fe7a4e4970a26a8a
Brandon Fenning <brandon@lform.com>
2021-12-10 13:33:52 -0500

    Fixes include path on postcss imports

--- postcss.config.js
+++ postcss.config.js
@@ -2,10 +2,7 @@ module.exports = {
 	parser: 'postcss-scss',
 	plugins: [
 		require('postcss-import')({
-			path: [
-				'./public/assets/tailwind/css',
-				'./node_modules/@lform/lwind/css',
-			],
+			path: ['./public/assets/css', './node_modules/@lform/lwind/css'],
 		}),
 		require('precss'),
 		require('postcss-rem'),
