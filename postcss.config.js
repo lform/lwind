c1bf16fcdeada5cbcd4e9ff3f38732f27e436d6d
Brandon Fenning <brandon@lform.com>
2021-12-15 22:15:26 -0500

    Adjusts postcss config for demo purposes

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,7 @@ module.exports = {
 	parser: 'postcss-scss',
 	plugins: [
 		require('postcss-import')({
-			path: ['./public/assets/css', './node_modules/@lform/lwind/css'],
+			path: ['./css', './node_modules/@lform/lwind/css'],
 		}),
 		require('precss'),
 		require('postcss-rem'),
