601c0f0d221df8838006cc19c2c0bfcb4f8e09c4
Brandon Fenning <brandon@lform.com>
2022-08-01 20:52:07 -0400

    Removes `precss` as package is no longer maintained, adds individual packages as `precss` was just a wrapper for those packages

--- postcss.config.js
+++ postcss.config.js
@@ -4,7 +4,12 @@ module.exports = {
 		require('postcss-import')({
 			path: ['./css', './node_modules/@lform/lwind/css'],
 		}),
-		require('precss'),
+		require('postcss-extend-rule'),
+		require('postcss-advanced-variables'),
+		require('postcss-preset-env'),
+		require('postcss-atroot'),
+		require('postcss-property-lookup'),
+		require('tailwindcss/nesting'),
 		require('postcss-rem'),
 		require('tailwindcss'),
 		require('autoprefixer'),
