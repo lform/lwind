2fad43e6d8cf0bedf036ccccd022d456ff6dd62b
bmf <innoscience@users.noreply.github.com>
2024-05-09 00:40:51 -0400

    Lwind 2.0 (#23) (#26)
    * Removes old `text-hms-*` header classes
    
    * Moves social media icons
    
    * Removes old images
    
    * Fixes bug with TailwindCSS imports
    
    * Fixes `postcss-import` rules
    
    * Removes javascript files
    
    * Removes NPM packages: `postcss-preset-env`, `postcss-property-lookup`, `postcss-scss`
    
    * Removes `lpress` styling
    
    * Adds `_layout.pcss` for sticky-footer components
    
    * Removes `tsconfig.json`
    
    * Removes package `postcss-rem`
    
    * Removes all `gulp` and related build packages
    
    * Updates `readme.md` with new build commands, removes `gulp` information

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,9 @@
 module.exports = {
-	parser: 'postcss-scss',
 	plugins: [
-		require('postcss-import')({
-			path: ['./css', './node_modules/@lform/lwind/css'],
-		}),
+		require('postcss-import'),
 		require('postcss-advanced-variables'),
 		require('postcss-atroot'),
 		require('postcss-extend-rule'),
-		require('postcss-preset-env'),
-		require('postcss-property-lookup'),
 		require('postcss-rem'),
 		require('tailwindcss/nesting'),
 		require('tailwindcss'),
